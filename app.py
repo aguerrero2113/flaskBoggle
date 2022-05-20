b439dd773e5443110a729e81267ec11b6fdfc50f
Artemio Guerrero <aguerrero2113@gmail.com>
2022-05-20 13:13:04 -0700

    modified boggle project

---
+++ app.py
@@ -0,0 +1,44 @@
+from urllib import response
+from flask import Flask,request,render_template,jsonify,session
+from boggle import Boggle
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "abcdefghijklmnopqrstuvwxyz"
+
+boggle_game = Boggle()
+
+@app.route("/")
+def homepage():
+    """Show board."""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+@app.route("/check-word")
+def check_word():
+    """check if valid word in dictionary"""
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board,word)
+
+    return jsonify({'result': response})
+
+@app.route("/post-score",methods=["POST"])
+def post_score():
+    """recieve score, update nplays, update high score if appropriate"""
+
+    score = request.json["score"]
+    highscore = session.get('highsore',0)
+    nplays = session.get("nplays",0)
+
+    session['nplays'] = nplays + 1
+    
+    session['highscore'] = max(score,highscore)
+
+    return jsonify(brokenRecord=score > highscore)    
